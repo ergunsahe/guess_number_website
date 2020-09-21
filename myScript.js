ac0c2c4a28038caeca0c50881d5aadcfff19455a
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-09-21 23:59:55 +0200

    feat:create a website to find which number is guessed

---
+++ myScript.js
@@ -0,0 +1,27 @@
+let  first= Math.floor((Math.random() * 100) + 1);
+let times_try = 1;
+document.getElementById("submit").addEventListener("click",
+function () {
+    
+    let second_num = document.getElementById("number").value;
+    let guess_num = parseInt(second_num);
+    
+    
+    if (first === guess_num) {
+        
+        document.getElementById("lowerupper").innerHTML =`You are the Winner`;
+        document.getElementById("tryingtimes").innerHTML =`You have tried ${times_try} times..`;
+        times_try += 1;
+    } else if (first > guess_num) {
+      
+        document.getElementById("lowerupper").innerHTML =`I am thinking upper then you`;
+        document.getElementById("tryingtimes").innerHTML =`You have tried ${times_try} times..`;
+        times_try += 1;
+    } else {
+     
+        document.getElementById("lowerupper").innerHTML =`I am thinking lower then you`;
+        document.getElementById("tryingtimes").innerHTML =`You have tried ${times_try} times..`;
+        times_try += 1;
+    }
+}
+)
